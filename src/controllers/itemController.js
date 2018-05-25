62275b3b06e8ab212ab7be362b5d7edc0be8cbd5
Leonardo Facundes <leonardo.fm00@gmail.com>
2018-05-25 17:06:59 -0300

    o que ta escrito no 'itemController.js' devia estar no './src/middlewares/auth.js'

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,1 +1,17 @@
-//TODO: Preencher este arquivo 
+//TODO: Preencher este arquivo
+const jwt = require('jsonwebtoken'),
+    authConfig = require('../config/auth');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({error: 'Nenhum token encontrado'});
+    }
+
+    const parts = authHeader.split(' ');
+
+    if(!parts.length === 2){
+        return 
+    }
+}
