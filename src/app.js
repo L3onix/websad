cd648964906b736b26c4f0d2dd64d082bc45107c
Leonardo Facundes <leonardo.fm00@gmail.com>
2018-05-24 22:50:51 -0300

    nodemon configurado e main js configurado

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//prevenindo problema de cross origin
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//request base
+app.get('/', (req, res) => {
+    res.status(200).send({"status": "ok"});
+});
+
+
+module.exports = app;
