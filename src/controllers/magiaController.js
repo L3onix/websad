8f7e798db21d0a0433c3a3355bbd1d9c7a959018
Leonardo Facundes <leonardo.fm00@gmail.com>
2018-06-07 16:32:24 -0300

    terminei rotas de magiaController

--- src/controllers/magiaController.js
+++ src/controllers/magiaController.js
@@ -2,9 +2,42 @@ const express = require('express'),
     router = express.Router(),
     Magia = require('../models/magia');
 
-//rota 
+//rota para buscar todas as magias
+router.get('/todas/', async (req, res) => {
+    //return res.status(200).send({ok: true});
+    try{
+        const magias = await Magia.find().populate('criador');
+
+        return res.status(200).send({magias});
+    }catch(err){
+        console.log(err);
+        return res.status(400).send({err: 'Erro ao carregar todas as magias'});
+    }
+});
+
+//rota para buscar todas as magias oficiais
 router.get('/', async (req, res) => {
-    return res.status(200).send({ok: true});
+    try{
+        const magias = await Magia.find({"oficial": true}).populate('criador');
+
+        return res.status(200).send({magias});
+    }catch(err){
+        console.log(err);
+        return res.status(400).send({err: 'Erro ao carregar magias oficiais'});
+    }
+});
+
+//TODO: checar se a magia recebia é oficial
+//rota para buscar uma magia
+router.get('/:magiaId', async (req, res) => {
+    try{
+        const magia = await Magia.findById(req.params.magiaId);
+
+        return res.status(200).send({magia});
+    }catch(err){
+        console.log(err);
+        return res.status(400).send({err: 'Erro ao carregar magia oficial'});
+    }
 });
 
 module.exports = app => app.use('/magia', router);
